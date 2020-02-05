35c57349528bc37f3a22b0ec26e0d4d2f849099d
alexanderyaguachi <44449414+alexanderyaguachi@users.noreply.github.com>
2020-02-04 22:38:27 -0500

    Add files via upload

---
+++ Deber_MSleep_Micros.ino
@@ -0,0 +1,44 @@
+
+  #include <TimerOne.h>
+  #include <avr/wdt.h>
+  #include<avr/sleep.h>
+  #include<avr/power.h>
+  #include<EEPROM.h>
+  int conteo = 0;
+  int valor;
+
+  void setup() {
+    Serial.begin(9600);
+    valor = EEPROM.read(0);
+    EEPROM.write(0, 1);
+    attachInterrupt(digitalPinToInterrupt(0), levantar, LOW);
+    Timer1.initialize(1000000);
+    Timer1.attachInterrupt(contador);
+    Serial.begin(9600);
+    Serial.println(valor);
+    }
+  void loop() {
+     if (conteo == 2 ) {
+    Serial.println("Sistema Activo");
+    Serial.println(conteo);
+    }
+     if (conteo == 4) {
+    Serial.println("Sistema en Reposo");
+    EEPROM.update(0, 0);
+    sleep_mode();
+    sleep_enable();
+    }  
+    if (conteo == 10) {
+    sleep_disable();
+    conteo = 0;
+     }
+   }
+  void contador() {
+    conteo++;
+  }
+  void levantar() {
+    sleep_disable();
+    Serial.println("Sistema Encendido");
+    conteo = 0;
+  }
+
